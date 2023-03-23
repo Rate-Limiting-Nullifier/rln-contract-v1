c859d0b9a18036edfc11045cf92032a17441266f
Magamedrasul Ibragimov <curryrasul@gmail.com>
2023-03-23 11:06:16 +0400

    feat: add changeFeeReceiver/Percentage functions

--- src/RLN.sol
+++ src/RLN.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.8.17;
 import {IPoseidonHasher} from "./PoseidonHasher.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title Rate-Limit Nullifier registry contract
 /// @dev This contract allows you to register RLN commitment and withdraw/slash.
-contract RLN {
+contract RLN is Ownable {
     using SafeERC20 for IERC20;
 
     /// @dev Membership deposit (stake amount) value.
@@ -20,13 +21,13 @@ contract RLN {
     uint256 public immutable SET_SIZE;
 
     /// @dev Address of the fee receiver.
-    address public immutable FEE_RECEIVER;
+    address public FEE_RECEIVER;
 
     /// @dev Fee percentage.
-    uint256 public constant FEE_PERCENTAGE = 5;
+    uint8 public FEE_PERCENTAGE;
 
     /// @dev Fee amount.
-    uint256 public immutable FEE;
+    uint256 public FEE_AMOUNT;
 
     /// @dev Current index where pubkey will be stored.
     uint256 public pubkeyIndex = 0;
@@ -57,12 +58,14 @@ contract RLN {
 
     /// @param membershipDeposit: Membership deposit;
     /// @param depth: Depth of the merkle tree;
+    /// @param feePercentage: Fee percentage;
     /// @param feeReceiver: Address of the fee receiver;
     /// @param _token: Address of the ERC20 contract;
     /// @param _poseidonHasher: Address of the Poseidon hasher contract.
     constructor(
         uint256 membershipDeposit,
         uint256 depth,
+        uint8 feePercentage,
         address feeReceiver,
         address _token,
         address _poseidonHasher
@@ -71,8 +74,9 @@ contract RLN {
         DEPTH = depth;
         SET_SIZE = 1 << depth;
 
+        FEE_PERCENTAGE = feePercentage;
         FEE_RECEIVER = feeReceiver;
-        FEE = FEE_PERCENTAGE * MEMBERSHIP_DEPOSIT / 100;
+        FEE_AMOUNT = FEE_PERCENTAGE * MEMBERSHIP_DEPOSIT / 100;
 
         token = IERC20(_token);
         poseidonHasher = IPoseidonHasher(_poseidonHasher);
@@ -136,12 +140,27 @@ contract RLN {
             token.safeTransfer(receiver, MEMBERSHIP_DEPOSIT);
             emit MemberWithdrawn(pubkey);
         } else {
-            token.safeTransfer(receiver, MEMBERSHIP_DEPOSIT - FEE);
-            token.safeTransfer(FEE_RECEIVER, FEE);
+            token.safeTransfer(receiver, MEMBERSHIP_DEPOSIT - FEE_AMOUNT);
+            token.safeTransfer(FEE_RECEIVER, FEE_AMOUNT);
             emit MemberSlashed(pubkey, receiver);
         }
 
         delete members[pubkey];
+    }
+
+    /// @dev Changes fee percentage.
+    ///
+    /// @param feePercentage: New fee percentage.
+    function changeFeePercentage(uint8 feePercentage) external onlyOwner {
+        FEE_PERCENTAGE = feePercentage;
+        FEE_AMOUNT = FEE_PERCENTAGE * MEMBERSHIP_DEPOSIT / 100;
+    }
+
+    /// @dev Changes fee receiver.
+    ///
+    /// @param feeReceiver: New fee receiver.
+    function changeFeeReceiver(address feeReceiver) external onlyOwner {
+        FEE_RECEIVER = feeReceiver;
     }
 
     /// @dev Returns Poseidon hash.
